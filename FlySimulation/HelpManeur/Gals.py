19ead80e06ec1e334370d1ec0567463f08580753
andyreigan <pavlovfmf@gmail.com>
2020-07-14 14:25:24 +0300

    fotm the structure of Gals

--- FlySimulation/HelpManeur/Gals.py
+++ FlySimulation/HelpManeur/Gals.py
@@ -1,7 +1,30 @@
 import math
 import _sqlite3
+
+
 class Gals:
-    def getPoint(self):
-        A=[]
+    def __init__(self, lp, bp, angleRot, rotate, dist):
+        self.lp = lp
+        self.bp = bp
+        self.angleRot = angleRot
+        self.rotate = rotate
+        self.dist = dist
+
+    def get(self):
+        A = []
+
+        lp = self.lp
+        bp = self.bp
+        angleRot = self.angleRot
+        rotate = self.rotate
+        dist = self.dist
 
         return A
+
+    def save(self):
+        # save object to sql DB
+        pass
+
+    def plot(self):
+        # generate plot
+        pass
