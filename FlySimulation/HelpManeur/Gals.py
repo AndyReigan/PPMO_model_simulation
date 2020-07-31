0d66bd97ace91ce0d730bee53134deec23e33143
andyreigan <pavlovfmf@gmail.com>
2020-07-31 12:47:39 +0300

    Исправил опечатки в галсировании теперь строит правильные зигзаги

--- FlySimulation/HelpManeur/Gals.py
+++ FlySimulation/HelpManeur/Gals.py
@@ -6,15 +6,16 @@ from Device.HelpFunc import HelpMethod
 from Structure.TrajectoryStruct import initialNavData
 import numpy as np
 import matplotlib.pyplot as plt
+# from basemap import Basemap
 
 class Gals:
-    def __init__(self, lp, bp, id):
+    def __init__(self, lp, bp, dist, rotate, id):
         self.lp = lp
         self.bp = bp
         self.angle_rot = id.azimut
-        self.rotate = id.status1
+        self.rotate = rotate
 
-        self.dist = id.dist
+        self.dist = dist
 
         self.id = id
 
@@ -34,14 +35,15 @@ class Gals:
         k = n + 2  # общее число ППМ
         # инициализируем необходимые нам точки
         a0_point = HelpMethod(self.id)
-
+        a0_point.dist = lp
+        a0_point.azimut = angle_rot
         b0_point = a0_point.direct_task_sphere()
         b0_point.dist = a0_point.dist
         b0_point = HelpMethod(b0_point)
         if rotate == "Left":
-            angle_rot + math.pi / 2
+            angle_rot = + math.pi / 2
         elif rotate == "Right":
-            angle_rot - math.pi / 2
+            angle_rot = - math.pi / 2
         a0_point.azimut = angle_rot
         b0_point.azimut = angle_rot
         i = 1
@@ -49,9 +51,20 @@ class Gals:
             a0_point.dist = i * dist
             b0_point.dist = i * dist
             current_point1 = a0_point.direct_task_sphere()
-            A.append(current_point1)
             current_point2 = b0_point.direct_task_sphere()
-            A.append(current_point2)
+
+
+            if i % 2 != 0:
+                current_point1.status1 = 'A' + str(i)
+                A.append(current_point1)
+
+                current_point2.status1 = 'B' + str(i)
+                A.append(current_point2)
+            elif i % 2 == 0:
+                current_point2.status1 = 'B' + str(i)
+                A.append(current_point2)
+                current_point1.status1 = 'A' + str(i)
+                A.append(current_point1)
             i = i + 1
         return A
 
@@ -64,18 +77,23 @@ class Gals:
         # generate plot
         pass
 
-        x=np.zeros(len(A))
-        y=np.zeros(len(A))
-        for i in range(0,len(A)):
-            x[i]=A[i].lat
-            y[i]=A[i].lon
+        x = np.zeros(len(A))
+        y = np.zeros(len(A))
+        name = []
+        for i in range(0, len(A)):
+            x[i] = A[i].lat
+            y[i] = A[i].lon
+            name.append(A[i].status1)
 
-        plt.plot(x,y)
+        plt.plot(x, y)
+        for i in range(0, len(x)):
+            plt.text(x[i], y[i], name[i])
         plt.show()
 
-m = initialNavData(lat=0.6, lon=0.7, azimut=0.7, dist=50/(6343*math.pi/2), status1='Left')
-a = Gals(lp=1000/(6343*math.pi/2), bp=100000/(6343*math.pi/2), id=m)
+
+m = initialNavData(lat=0.6, lon=0.72, azimut=0.2)
+a = Gals(lp=1000 / (6343 * math.pi / 2), bp=1000 / (6343 * math.pi / 2), dist=200 / (6343 * math.pi / 2), rotate='Left',
+         id=m)
 diction = a.get()
 Gals.plot(diction)
 print(diction)
-
